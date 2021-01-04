933fe85173854a5a747115c7cc301a47ed272a88
Joel-Zwaan <74177925+Joel-Zwaan@users.noreply.github.com>
2021-01-04 10:52:56 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,21 @@
-// JavaScript Document
+var menuButton = document.querySelector('#hamburger-menu');
+var nav = document.getElementById('navigatie');
+
+    
+menuButton.addEventListener('click', toonMenu)
+
+
+function toonMenu() {
+    nav.classList.toggle('toon-menu');
+}
+var image_tracker = 'H'
+function change(){
+    var image = document.getElementById('menu')
+    if(image_tracker=='H'){
+        image.src ='images/cross.svg';
+        image_tracker = 'c'
+    }else{
+        image.src = 'images/Hamburger.svg';
+        image_tracker ='H';
+    }
+}
